a51eb0876b772be096fb61954d1dcf916ca55735
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-08 23:03:22 +0900

    commenting further

--- src/process/resolve/detect/containsDate.js
+++ src/process/resolve/detect/containsDate.js
@@ -1,10 +1,10 @@
 /**
- * 
+ * Judges wether the string line contains date sequence.
+ * date sequence can be both: 0000-00-00 and 00-00-0000
  * @param {string} line 
  * @return {boolean} 
  */
 function isDate(line){
-  //e.g. both matches 2014/02/20 <-> 20/02/2014 
   var exp = new RegExp(/.*[0-9]{1,4}\-[0-9]{1,2}\-[0-9]{1,4}.*/);
   if(line.match(exp)===null){
     return false;
