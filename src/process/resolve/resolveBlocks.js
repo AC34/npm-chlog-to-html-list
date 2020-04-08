4465da4c3c18fefeb27fb5ce2dd6322318ffdcf2
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-08 23:00:24 +0900

    commenting further

--- src/process/resolve/resolveBlocks.js
+++ src/process/resolve/resolveBlocks.js
@@ -1,8 +1,11 @@
 /**
+ * resolves content dependencies between objects.
+ * e.g. link object will be merged by its matching object by version.
  * @param {object} obj 
  * @return {object}
  */
 function resolveBlocks(obj){
+  //prepare
   var mergeLinks = require("./mergeLinks");
   var resolveDates = require("./resolveDates");
   //merge links to each version's objects
