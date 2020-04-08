178c75cad038a6287b23a041f640252ab4161ecc
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-08 23:02:37 +0900

    commenting further

--- src/process/resolve/parse/parseDate.js
+++ src/process/resolve/parse/parseDate.js
@@ -1,9 +1,12 @@
 /**
- * 
+ * Parses date sequence.
+ * both 0000-00-00 and 00-00-0000 can be parsed.
+ * matching pattern is very loose(in case of changelog typos)
  * @param {string} date_text 
  * @param {string} date_string
  */
 function parseDate(date_text){
+
   var exp = new RegExp(/[0-9]{1,4}\-[0-9]{1,2}\-[0-9]{1,4}/);
   return date_text.match(exp);
 }
