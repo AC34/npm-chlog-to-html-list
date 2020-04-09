75baacef93d93d93383477569e8c5341d0407dc3
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-09 14:25:49 +0900

    formatting path

--- src/util/getPackageJson.js
+++ src/util/getPackageJson.js
@@ -1,6 +1,9 @@
 function getPackageJson(project_root, package_json_path, msgs) {
-  //project_root is always not undefined
+  //formatting
   package_json_path = !package_json_path ? "package.json" : package_json_path;
+  package_json_path = package_json_path ===""? "package.json" : package_json_path;
+  package_json_path = package_json_path.startsWith("./")?package_json_path.replace("./",""):package_json_path;
+  package_json_path = package_json_path.startsWith("/")?package_json_path.replace("/",""):package_json_path;
   var sep = require("path").sep;
   var jpath = project_root + sep + package_json_path;
   var json = require("fs").readFileSync(jpath, "UTF-8");
