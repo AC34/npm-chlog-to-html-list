5c0dc62a10ff8eb6aff5afe253b0a6c96cb236ac
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-09 15:23:17 +0900

    formatting log path

--- src/util/getChangelogFile.js
+++ src/util/getChangelogFile.js
@@ -1,6 +1,11 @@
 function getChangelogFile(project_root, changelog_path="", msgs) {
   var sep = require("path").sep;
+  //formatting
   changelog_path = !changelog_path ? "CHANGELOG.md" : changelog_path;
+  changelog_path = changelog_path==="" ? "CHANGELOG.md" : changelog_path;
+  changelog_path = changelog_path.startsWith("./")?changelog_path.replace("./",""):changelog_path;
+  changelog_path = changelog_path.startsWith("/")?changelog_path.replace("/",""):changelog_path;
+  //making path
   var cpath = project_root + sep + changelog_path;
   if (!require("fs").existsSync(cpath)) {
     console.log(msgs["file-not-found"]({ path: cpath }));
