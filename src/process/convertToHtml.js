1d1dc37fd2c51948f4cf41e17511987a4cb3c2b9
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-10 14:19:05 +0900

    removed duplicated lines

--- src/process/convertToHtml.js
+++ src/process/convertToHtml.js
@@ -11,10 +11,6 @@ function convertToHtml(parsed_object,args,msgs){
   for(var i in parsed_object){
     var obj = parsed_object[i];
     var section = "";
-    //version
-    if(obj.version_text){
-      section+=makeHtml(args.section_header_element,obj.version_text,{});
-    }
     //version_text 
     if(obj.version_text){
       section+=makeHtml(args.section_header_element,obj.version_text,{});
@@ -46,10 +42,10 @@ function convertToHtml(parsed_object,args,msgs){
     }
     //link
     if(obj.link){
-      section+=makeHtml(args.entry_element,obj.link,{});
+      section+=makeHtml(args.link_element,obj.link,{});
     }
     //wrap by section
-    section = makeHtml(args.section_elemenet,section,{});
+    section = makeHtml(args.section_element,section,{});
     //var section = makeHtml(args.section_element,"",{});
     //wrap by li
     section = makeHtml("li",section,{});
