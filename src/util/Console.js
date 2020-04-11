30cd1af612960f8ae436835633f363e67e3d4816
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-11 19:30:48 +0900

    updating

--- src/util/Console.js
+++ src/util/Console.js
@@ -3,14 +3,23 @@ function Console(){
   this.msgs = undefined; 
   this.verbose = true;
 }
-
+/**
+ * 
+ */
 Console.prototype.prepare=function(args){
   if(args.verbose){
     this.verbose = args.verbose;
   } 
 }
-Console.prototype.log = function(msg_key){
-    
+/**
+ * @param {msg_key} string
+ * @param {object} args
+ */
+Console.prototype.log = function(msg_key,args){
+  var msg = this.msgs[msg_key](args);
+  this.log.push(msg); 
+  if(this.verbose===true){
+    console.log(msg);
+  }
 }
-
 module.exports = Console;
