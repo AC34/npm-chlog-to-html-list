c3b7eed2ea1c703bfeea751f7fd666ecda48c510
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-11 22:55:19 +0900

    console to Console

--- src/util/getPackageJson.js
+++ src/util/getPackageJson.js
@@ -10,14 +10,14 @@ function getPackageJson(args, Console) {
   var jpath = args.project_root + sep + package_json_path;
   var json = require("fs").readFileSync(jpath, "UTF-8");
   if (!require("fs").existsSync(jpath)) {
-    console.log(msgs["file-not-found"]({ path: jpath }));
+    Console.log("file-not-found",{path:jpath});
   }
   try {
     var pj = JSON.parse(json);
     //success
     return pj;
   } catch (e) {
-    console.log(msgs["packagejson-parse-failed"]());
+    Console.log("packagejson-parse-failed",());
   }
 }
 module.exports = getPackageJson;
