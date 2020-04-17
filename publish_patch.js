705b844ad207c09b561c1c26acdd842ae658c615
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-04-17 23:07:43 +0900

    removing debug lone

--- publish_patch.js
+++ publish_patch.js
@@ -1,10 +1,8 @@
 var exec = require('child_process');
-var sep = rquire("path").sep;
 
 //embed changelog to readme
+require("./publish/embed_log");
 
-
-process.exit(0);
 //update installed modules
 exec.exec('npm install',(err,stdout,stderror)=>{
   console.log(stdout);
